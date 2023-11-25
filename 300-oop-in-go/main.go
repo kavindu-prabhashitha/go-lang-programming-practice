4e379aaeaee46157fe7f125afd7514a90d529087
Kavindu Prabhashitha <donprabhashitha@gmail.com>
2023-11-25 11:59:20 +0530

    oop-in-go

---
+++ 300-oop-in-go/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "go-tutorial/300-oop-in-go/employee"
+
+func main() {
+	e := employee.Employee{
+		FirstName:   "Sam",
+		LastName:    "Adolf",
+		TotalLeaves: 30,
+		LeavesTaken: 20,
+	}
+	e.LeavesRemaining()
+}
